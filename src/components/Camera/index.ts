f20f8aba3fe2c6d9944263b16971d3b5584b15a1
Yonko Draganov <yonko.draganov@trading212.com>
2026-02-07 11:14:10 +0200

    Plan max

--- src/components/Camera/index.ts
+++
@@ -1,1 +0,0 @@
-export { CameraController } from './CameraController';
--- src/components/UI/index.ts
+++
@@ -1,6 +0,0 @@
-export { Pointer3D } from './Pointer';
-export { Overlay } from './Overlay';
-export { HandVisualization } from './HandVisualization';
-export { VoiceIndicator } from './VoiceIndicator';
-export { StatusPanel } from './StatusPanel';
-export { Instructions } from './Instructions';
